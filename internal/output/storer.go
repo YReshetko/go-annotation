6100b6f348350415290ed8b99d1e6da46e7f7e9c
Yury Rash <yrashetska@gmail.com>
2022-06-19 11:35:10 +0300

    Formatting go code

--- internal/output/storer.go
+++ internal/output/storer.go
@@ -1,16 +1,30 @@
 package output
 
 import (
-	"github.com/YReshetko/go-annotation/internal/environment"
+	"go/format"
 	"os"
+
+	"github.com/YReshetko/go-annotation/internal/environment"
 )
 
 func Save(_ environment.Arguments, data map[string][]byte) error {
 	for path, content := range data {
+		if isGoFile(path) {
+			formattedContent, err := format.Source(content)
+			if err != nil {
+				return err
+			}
+			content = formattedContent
+		}
+
 		if err := os.WriteFile(path, content, os.ModePerm); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func isGoFile(path string) bool {
+	return path[len(path)-3:] == ".go"
 }
