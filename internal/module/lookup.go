9332c641b64c947cb1148ca20edea1cb741a4f76
Yury Rash <yrashetska@gmail.com>
2024-10-20 15:35:51 +0300

    Use standard packages lookup

--- internal/module/lookup.go
+++ internal/module/lookup.go
@@ -1,105 +1,30 @@
 package module
 
 import (
+	"context"
 	"fmt"
 	"path/filepath"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
-
-	"github.com/YReshetko/go-annotation/internal/environment"
-	"github.com/YReshetko/go-annotation/internal/logger"
+	"golang.org/x/tools/go/packages"
 )
 
-var _ moduleLookup = (*selfLookup)(nil)
-var _ moduleLookup = (*subLookup)(nil)
-var _ moduleLookup = (*dependencyLookup)(nil)
 var _ moduleLookup = (*stdLookup)(nil)
-var _ moduleLookup = (*workspaceLookup)(nil)
-var _ moduleLookup = (*vendorLookup)(nil)
 var _ moduleLookup = (*chainLookup)(nil)
+var _ moduleLookup = (*packagesLookup)(nil)
 
 var lookup moduleLookup
 
 func init() {
 	cl := &chainLookup{}
-	cl.add(selfLookup{})
-	cl.add(subLookup{})
-	//cl.add(workspaceLookup{})
-	cl.add(dependencyLookup{})
-	cl.add(stdLookup{})
-	//cl.add(vendorLookup{})
-
+	cl.add(&packagesLookup{})
+	cl.add(&stdLookup{})
 	lookup = cl
 }
 
 type moduleLookup interface {
 	find(m *module, importPath string) (*module, error)
-}
-
-type selfLookup struct{}
-
-func (s selfLookup) find(m *module, importPath string) (*module, error) {
-	if !m.hasModFile() {
-		if m.hasImportPath(importPath) {
-			return m, nil
-		}
-		return nil, nil
-	}
-
-	if m.isSubModule(importPath) {
-		return nil, nil
-	}
-
-	if strings.Contains(importPath, moduleName(m.mod)) {
-		return m, nil
-	}
-	return nil, nil
-}
-
-type subLookup struct{}
-
-func (s subLookup) find(m *module, importPath string) (*module, error) {
-	modPath := m.subModuleRoot(importPath)
-	if len(modPath) == 0 {
-		return nil, nil
-	}
-
-	// TODO Make it cacheable
-	mod, err := moduleLoader.load(filepath.Dir(modPath))
-	if err != nil {
-		return nil, fmt.Errorf("unable to preload submodule by import %s: %w", importPath, err)
-	}
-	return &mod, nil
-}
-
-type dependencyLookup struct{}
-
-func (d dependencyLookup) find(m *module, importPath string) (*module, error) {
-	if !m.hasModFile() {
-		return nil, nil
-	}
-
-	mn := m.findClosestModuleName(importPath)
-	if len(mn) == 0 {
-		return nil, nil
-	}
-
-	// Preload and return module
-	path, ok := m.escapedPath(mn)
-	if !ok {
-		logger.Warnf("closest module was found: %s, but unable to build escaped path", mn)
-		return nil, nil
-	}
-	modulePath := filepath.Join(environment.ModPath(), path)
-
-	// TODO make it cacheable
-	subModule, err := moduleLoader.load(modulePath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to preload submodule %s, due to: %w", importPath, err)
-	}
-
-	return &subModule, nil
 }
 
 type stdLookup struct{}
@@ -119,20 +44,6 @@ func (s stdLookup) find(_ *module, importPath string) (*module, error) {
 	return nil, nil
 }
 
-type workspaceLookup struct{}
-
-func (w workspaceLookup) find(*module, string) (*module, error) {
-	logger.Warn("workspace module lookup is not implemented, yet")
-	return nil, nil
-}
-
-type vendorLookup struct{}
-
-func (v vendorLookup) find(*module, string) (*module, error) {
-	logger.Warn("vendor module lookup is not implemented, yet")
-	return nil, nil
-}
-
 type chainLookup struct {
 	lookup   moduleLookup
 	fallback *chainLookup
@@ -149,6 +60,9 @@ func (c *chainLookup) find(m *module, importPath string) (*module, error) {
 	}
 
 	cmod, chainErr := c.fallback.find(m, importPath)
+	if cmod != nil {
+		return cmod, nil
+	}
 	err := multierror.Append(lookupErr, chainErr)
 	return cmod, err.ErrorOrNil()
 }
@@ -163,4 +77,31 @@ func (c *chainLookup) add(lookup moduleLookup) {
 		c.fallback = &chainLookup{}
 	}
 	c.fallback.add(lookup)
+}
+
+type packagesLookup struct{}
+
+func (p packagesLookup) find(_ *module, importPath string) (*module, error) {
+	pkgs, err := packages.Load(&packages.Config{
+		Mode:    packages.NeedModule,
+		Context: context.Background(),
+	}, importPath)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to load package for %s import: %w", importPath, err)
+	}
+
+	if len(pkgs) == 0 {
+		return nil, fmt.Errorf("module for import '%s' not found", importPath)
+	}
+	if len(pkgs) > 1 {
+		return nil, fmt.Errorf("found more than one module for import '%s': %v", importPath, pkgs)
+	}
+
+	if pkgs[0].Module == nil {
+		return nil, fmt.Errorf("trying to load non-module pakage: %s", importPath)
+	}
+
+	subModule, err := moduleLoader.load(pkgs[0].Module.Dir)
+	return &subModule, nil
 }
